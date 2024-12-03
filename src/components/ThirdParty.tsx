34eebeb2b0952fde3f143eb7a52ffa0497672f3b
Apichet Komwatcharapong <apichet@thinknet.co.th>
2024-12-03 15:41:21 +0700

    update

--- src/components/ThirdParty.tsx
+++ src/components/ThirdParty.tsx
@@ -1,6 +1,13 @@
 'use client';
 import { Partytown } from '@builder.io/partytown/react';
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
 export function ThirdParty() {
   return (
     <>
@@ -10,17 +17,18 @@ export function ThirdParty() {
         lib="/~partytown/"
       />
       <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-M8QWC2M7"
         type="text/partytown"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            '/api/gtm?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M8QWC2M7');
-          `
-        }}
-      />
+      ></script>
+      <script type="text/partytown">
+        window.dataLayer = window.dataLayer || [];
+        window.gtag = function () {
+          window.dataLayer.push(arguments)
+        };
+        window.gtag('js', new Date());
+        window.gtag('config', 'GTM-M8QWC2M7');
+      </script>
     </>
   );
 }
