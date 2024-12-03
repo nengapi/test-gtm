b0599a2f493fdf3f0f050fa1561d480f769ba895
Apichet Komwatcharapong <apichet@thinknet.co.th>
2024-12-03 16:25:20 +0700

    update cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: async () => {
+  async headers() {
     return [
       {
         source: "/~partytown/:path*",
@@ -8,6 +8,24 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Origin",
             value: "*",
+          },
+        ],
+      },
+      {
+        // Add headers for Google Tag Manager proxy
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.googletagmanager.com",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
           },
         ],
       },
--- src/components/ThirdParty.tsx
+++ src/components/ThirdParty.tsx
@@ -8,17 +8,25 @@ export function ThirdParty() {
         debug={true}
         forward={['dataLayer.push']}
         lib="/~partytown/"
+        resolveUrl={(url, location, type) => {
+          if (type === 'script' && url.hostname.includes('googletagmanager.com')) {
+            const proxyUrl = new URL(url);
+            proxyUrl.searchParams.append('gtm', 'true');
+            return proxyUrl;
+          }
+          return url;
+        }}
       />
-        <script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=GTM-M8QWC2M7"></script>
-        <script type="text/partytown" dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            window.gtag = function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
+      <script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=GTM-M8QWC2M7"></script>
+      <script type="text/partytown" dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          window.gtag = function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
 
-            gtag('config', 'GTM-M8QWC2M7');
-          `,
-        }} />
+          gtag('config', 'GTM-M8QWC2M7');
+        `,
+      }} />
     </>
   );
 }
