91437fc052abdfacaec883fac1706eb8e7c68c54
Apichet Komwatcharapong <apichet@thinknet.co.th>
2024-12-03 15:45:53 +0700

    update script

--- src/components/ThirdParty.tsx
+++ src/components/ThirdParty.tsx
@@ -1,13 +1,6 @@
 'use client';
 import { Partytown } from '@builder.io/partytown/react';
 
-declare global {
-  interface Window {
-    dataLayer: any[];
-    gtag: (...args: any[]) => void;
-  }
-}
-
 export function ThirdParty() {
   return (
     <>
@@ -21,14 +14,19 @@ export function ThirdParty() {
         src="https://www.googletagmanager.com/gtag/js?id=GTM-M8QWC2M7"
         type="text/partytown"
       ></script>
-      <script type="text/partytown">
-        window.dataLayer = window.dataLayer || [];
-        window.gtag = function () {
-          window.dataLayer.push(arguments)
-        };
-        window.gtag('js', new Date());
-        window.gtag('config', 'GTM-M8QWC2M7');
-      </script>
+      <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            window.gtag = function () {
+              dataLayer.push(arguments);
+            };
+            window.gtag('js', new Date());
+            window.gtag('config', 'GTM-M8QWC2M7');
+          `,
+        }}
+      />
     </>
   );
 }
