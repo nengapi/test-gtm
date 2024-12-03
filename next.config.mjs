f32db5bb8593ddcc4cfd76c02ca5cc050f1cb27d
Apichet Komwatcharapong <apichet@thinknet.co.th>
2024-12-03 15:15:35 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: async () => {
+    return [
+      {
+        source: "/~partytown/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- src/components/ThirdParty.tsx
+++ src/components/ThirdParty.tsx
@@ -1,22 +1,26 @@
 'use client';
 import { Partytown } from '@builder.io/partytown/react';
-import {
-  GTMScript,
-  GTM_TAG_ASSISTANT_ACCESSOR,
-  GTM_TAG_ASSISTANT_FORWARDER,
-  partytownResolveUrl
-} from '@superside-oss/partytown-gtm';
 
 export function ThirdParty() {
   return (
     <>
       <Partytown
         debug={true}
-        forward={['dataLayer.push', GTM_TAG_ASSISTANT_FORWARDER]}
-        mainWindowAccessors={[GTM_TAG_ASSISTANT_ACCESSOR]}
-        resolveUrl={partytownResolveUrl}
+        forward={['dataLayer.push']}
+        lib="/~partytown/"
       />
-      <GTMScript gtmId={'GTM-M8QWC2M7'} />
+      <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M8QWC2M7');
+          `
+        }}
+      />
     </>
   );
 }
