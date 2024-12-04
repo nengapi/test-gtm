ce577430aa4d9b6b881090e682a1c09f4c4779cb
Apichet Komwatcharapong <apichet@thinknet.co.th>
2024-12-04 14:13:53 +0700

    update

--- src/app/api/third-party/route.ts
+++ src/app/api/third-party/route.ts
@@ -1,74 +1,74 @@
-// import { isUrlAllowed } from '@superside-oss/partytown-gtm/util';
+import { isUrlAllowed } from '@superside-oss/partytown-gtm/util';
 
-// const PASS_THROUGH_HEADERS = [
-//   'content-type',
-//   'cache-control',
-//   'pragma',
-//   'expires',
-//   'date',
-//   'x-gtm-versionid',
-//   'content-security-policy',
-//   'x-xss-protection',
-//   'x-frame-options',
-//   'x-content-type-options'
-// ];
+const PASS_THROUGH_HEADERS = [
+  'content-type',
+  'cache-control',
+  'pragma',
+  'expires',
+  'date',
+  'x-gtm-versionid',
+  'content-security-policy',
+  'x-xss-protection',
+  'x-frame-options',
+  'x-content-type-options'
+];
 
-// function parseUrl(url: any): URL | null {
-//   try {
-//     return new URL(url);
-//   } catch (err) {}
+function parseUrl(url: any): URL | null {
+  try {
+    return new URL(url);
+  } catch (err) {}
 
-//   return null;
-// }
+  return null;
+}
 
-// async function getResponse(maybeUrl: string | null) {
-//   if (!maybeUrl) {
-//     throw new Error('URL is not set');
-//   }
+async function getResponse(maybeUrl: string | null) {
+  if (!maybeUrl) {
+    throw new Error('URL is not set');
+  }
 
-//   const url = parseUrl(decodeURIComponent(maybeUrl));
+  const url = parseUrl(decodeURIComponent(maybeUrl));
 
-//   if (!(url && isUrlAllowed(url))) {
-//     throw new Error('URL is not on the whitelist');
-//   }
+  if (!(url && isUrlAllowed(url))) {
+    throw new Error('URL is not on the whitelist');
+  }
 
-//   const response = await fetch(url);
+  const response = await fetch(url);
 
-//   if (response.status >= 400) {
-//     throw new Error('Invalid response status code');
-//   }
+  if (response.status >= 400) {
+    throw new Error('Invalid response status code');
+  }
 
-//   const data = await response.text();
-//   const headers: { [key: string]: string } = {};
+  const data = await response.text();
+  const headers: { [key: string]: string } = {};
 
-//   PASS_THROUGH_HEADERS.forEach((headerName) => {
-//     const headerValue = response.headers.get(headerName);
+  PASS_THROUGH_HEADERS.forEach((headerName) => {
+    const headerValue = response.headers.get(headerName);
 
-//     if (headerValue) {
-//       headers[headerName] = headerValue;
-//     }
-//   });
+    if (headerValue) {
+      headers[headerName] = headerValue;
+    }
+  });
 
-//   return new Response(data, {
-//     status: response.status,
-//     headers
-//   })
-// }
+  return new Response(data, {
+    status: response.status,
+    headers
+  })
+}
 
-// export async function GET(req: Request) {
-//   const originalUrl = new URL(req.url);
-//   const forwardUrl = originalUrl.searchParams.get('forward');
+export async function GET(req: Request) {
+  const originalUrl = new URL(req.url);
+  const forwardUrl = originalUrl.searchParams.get('forward');
 
-//   try {
-//     const response = await getResponse(forwardUrl);
+  try {
+    const response = await getResponse(forwardUrl);
 
-//     return response;
-//   } catch (err) {
-//     return new Response('Not found', {
-//       status: 404,
-//       headers: {
-//         'Content-Type': 'text/plain'
-//       }
-//     });
-//   }
-// }
+    return response;
+  } catch (err) {
+    return new Response('Not found', {
+      status: 404,
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    });
+  }
+}
--- src/components/ThirdParty.tsx
+++ src/components/ThirdParty.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-// import { GoogleTagManager } from "@next/third-parties/google";
-
 import { Partytown } from "@builder.io/partytown/react";
 import {
   GTM_TAG_ASSISTANT_ACCESSOR,
@@ -13,39 +11,13 @@ import {
 export function ThirdParty() {
   return (
     <>
-      {/* <GoogleTagManager gtmId="GTM-M8QWC2M7" /> */}
-      {/* <Partytown
-        debug={true}
-        forward={['dataLayer.push']}
-        lib="/~partytown/"
-        resolveUrl={(url, location, type) => {
-          if (type === 'script' && url.hostname.includes('googletagmanager.com')) {
-            const proxyUrl = new URL(url);
-            proxyUrl.searchParams.append('gtm', 'true');
-            return proxyUrl;
-          }
-          return url;
-        }}
-      />
-      <script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=GTM-M8QWC2M7"></script>
-      <script type="text/partytown" dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          window.gtag = function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'GTM-M8QWC2M7');
-        `,
-      }} /> */}
       <Partytown
         debug={true}
-        forward={[
-          "dataLayer.push", // Forward dataLayer.push
-        ]}
-        mainWindowAccessors={[]}
+        forward={[GTM_TAG_ASSISTANT_FORWARDER]}
+        mainWindowAccessors={[GTM_TAG_ASSISTANT_ACCESSOR]}
         resolveUrl={partytownResolveUrl}
       />
-      <GTMScript gtmId={"GTM-M8QWC2M7"} />
+      <GTMScript skipPartytown={true} gtmId={"GTM-M8QWC2M7"} />
     </>
   );
 }
